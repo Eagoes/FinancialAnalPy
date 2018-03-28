be571704318c94dba6b7bbcc23fb4b2baaf72d21
Eric K. Jobs <445039992@qq.com>
2018-03-28 10:21:31 +0800

    update 5

--- src/ImgDrawer.py
+++ src/ImgDrawer.py
@@ -28,3 +28,24 @@ def img_draw(title: str, category: list,  plot_params: list):
     fig.savefig(imgdata, format='png')
     imgdata.seek(0)
     return imgdata
+
+def bar_and_plot(categoty: list, bar_param: list, plot_param:list):
+    """
+    the function that draws an image contains a bar axis and a plot axis
+    :param categoty: data of the x axis
+    :param bar_param: data of the bar axis, bar title
+    :param plot_param: data of the plot axis, plot title
+    :return: the byte stream of the image
+    """
+    imgdata = BytesIO()
+    fig, left_axe = plt.subplots()
+    right_axe = left_axe.twinx()
+    left_axe.bar(categoty, bar_param[0], label=bar_param[1])
+    right_axe.plot(categoty, plot_param[0], label=plot_param[1])
+
+    box = left_axe.get_position()
+    left_axe.set_position([box.x0, box.y0 + box.height * 0.1,
+                      box.width, box.height * 0.9])
+    left_axe.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fontsize=9, frameon=False)
+    left_axe.ylabel(bar_param[1])
+    right_axe.ylabel(plot_param[1])
